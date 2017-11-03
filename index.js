f33b3d8c995557547d5359cdd1c3ee2889cab488
Andrey <rojer9999@gmail.com>
2017-11-03 18:13:05 +0200

    start

---
+++ index.js
@@ -0,0 +1,14 @@
+function binarySearch(num) {
+    // console.log('Math: ', Math.round(Math.log2(num)));
+    let counter = 0;
+    while (num / 2 > 0) {
+        if (num === 1) {
+            break;
+        } else {
+            num = Math.round(num / 2);
+        }
+        counter++;
+    }
+    console.log(counter);
+}
+binarySearch(2000000000);
