6d02647363710f6a79b44de3ea47e1bd9b2561ec
Andrey <rojer9999@gmail.com>
2017-11-15 19:45:27 +0200

    breadth ready

--- 5_breadthFirstSearch.js
+++ 5_breadthFirstSearch.js
@@ -1,30 +1,32 @@
-let d = {
-    name: 'Andrey',
-    real: false
-};
-let e = {
-    name: 'Yuriy',
-    real: false
-};
-let f = {
-    name: 'Sergey',
-    real: false
-};
+const graph = {};
 
-let a = {
-    name: 'Bob',
-    child: d,
-    real: false
-};
-let b = {
-    name: 'John',
-    child: e,
-    real: true
-};
-let c = {
-    name: 'Sam',
-    child: f,
-    real: false
-};
+graph['you'] = ['Bob', 'Alex', 'John'];
+graph['Bob'] = ['April'];
+graph['Alex'] = ['Sam', 'Sussy', 'April'];
+graph['John'] = ['Duke', 'Eva'];
 
-function breadthFirstSearch(obj) {}
+function isItYou(name) {
+    // for exsample
+    return name[name.length - 1] === 'y' ? true : false;
+}
+
+function breadthFirstSearch(name) {
+    let searched = [];
+    let queue = [name];
+    while (queue.length != 0) {
+        if (isItYou(queue[0])) {
+            return queue[0];
+        } else {
+            if (Array.isArray(graph[queue[0]])) {
+                for (let k = 0; k < graph[queue[0]].length; k++) {
+                    queue.push(graph[queue[0]][k]);
+                }
+            } else {
+                queue.push(queue[0]);
+            }
+            searched.push(queue.shift());
+        }
+    }
+}
+
+console.log(breadthFirstSearch('you'));
